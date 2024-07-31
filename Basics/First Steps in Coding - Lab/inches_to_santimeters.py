e0bfa0f8516a231b9c6b5b07757bcff21d19949f
mmilushev91 <milushev_1990@abv.bg>
2024-07-31 11:47:33 +0300

    Create inches_to_santimeters.py

---
+++ Basics/First Steps in Coding - Lab/inches_to_santimeters.py
@@ -0,0 +1,13 @@
+# Pseudocode
+
+# prompt user for inches (real number)
+# calculate santimeters by multiplying inches by 2.54
+# print santimeters
+
+INCH = 2.54
+
+inches = float(input())
+
+santimeters = inches * INCH
+
+print(santimeters)
