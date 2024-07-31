bb0c66d85471efcea9a5c725759e47daecea2b9c
mmilushev91 <milushev_1990@abv.bg>
2024-07-31 11:43:57 +0300

    Create triangle_area.py

---
+++ Basics/First Steps in Coding - Lab/triangle_area.py
@@ -0,0 +1,13 @@
+# Pseudocode
+
+# prompt user for a side length
+# prompt user for b side length
+# multipply a * b to calculate area
+# print area
+
+a = int(input())
+b = int(input())
+
+area = a * b
+
+print(area)
