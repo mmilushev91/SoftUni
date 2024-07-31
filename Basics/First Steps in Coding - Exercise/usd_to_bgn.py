d4b919d7a864fe122124219f31e17dc3a1fd20ed
mmilushev91 <milushev_1990@abv.bg>
2024-07-31 13:53:38 +0300

    Create usd_to_bgn.py

---
+++ Basics/First Steps in Coding - Exercise/usd_to_bgn.py
@@ -0,0 +1,15 @@
+# Pseudocode
+
+# prompt for usd(float)
+# init const variable usd to bgn price
+# calculate bgn price
+# print result
+
+
+usd = float(input())
+
+USD_TO_BGN_PRICE = 1.79549 
+
+bgn = usd * USD_TO_BGN_PRICE
+
+print(bgn)
