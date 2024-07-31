1044cbec9d2afc96cd44e7088c9e47c63e237b3c
mmilushev91 <milushev_1990@abv.bg>
2024-07-31 12:06:32 +0300

    Create concatenate_data.py

---
+++ Basics/First Steps in Coding - Lab/concatenate_data.py
@@ -0,0 +1,10 @@
+# Pseudocode
+# prompt user for first name (string), last name (string), age (int), town (string)
+# print message with f string
+
+first_name = input()
+last_name = input()
+age = int(input())
+town = input()
+
+print(f"You are {first_name} {last_name}, a {age}-years old person from {town}.")
