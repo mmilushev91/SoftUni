e5cf8dee8d098a632831c53cad06baae67812374
mmilushev91 <milushev_1990@abv.bg>
2024-10-30 14:18:27 +0200

    Update vacation_book_list.py

--- Basics/First Steps in Coding - Exercise/vacation_book_list.py
+++ Basics/First Steps in Coding - Exercise/vacation_book_list.py
@@ -1,13 +1,9 @@
-# Pseudocode
-
-# prompt user for book_pages(int), pages_per_hour(int), days(itn)
-# calculate hours_per_day with formula (book_pages / days) / pages_per_hour and trunc the result
-# print result
-
-book_pages = int(input())
-days = int(input())
+pages_to_read = int(input())
 pages_per_hour = int(input())
+days_to_read = int(input())
 
-hours_per_day = int((book_pages / days) / pages_per_hour)
+pages_per_day = pages_to_read / days_to_read
 
-print(hours_per_day)
+hours_per_day = pages_per_day / pages_per_hour
+
+print(int(hours_per_day))
