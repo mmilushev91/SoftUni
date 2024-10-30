425adc7f5da5778e937c0cc22c1b570b1c93f437
mmilushev91 <milushev_1990@abv.bg>
2024-10-30 12:22:03 +0200

    Create password_guess.py

---
+++ Basics/Conditional Statements - Lab/password_guess.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+if num % 2 == 0:
+  print("even")
+else:
+  print("odd")
+
+PASSWORD = "s3cr3t!P@ssw0rd"
+
+user_input = input()
+
+if PASSWORD == user_input:
+  print("Welcome")
+else:
+  print("Wrong password!")
