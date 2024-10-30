79eecd161a6764c0fe98ae832bf4fab0f0afc28e
mmilushev91 <milushev_1990@abv.bg>
2024-10-30 14:10:58 +0200

    Update pet_shop.py

--- Basics/First Steps in Coding - Lab/pet_shop.py
+++ Basics/First Steps in Coding - Lab/pet_shop.py
@@ -1,15 +1,8 @@
-# Pseudocode
-
-# prompt user for number of dog and cat food packets
-# init const variables for dog and cat packet price
-# calculate dog price, cat price and total price
-# print f string: "{крайната сума} lv."
+DOG_PACKET_PRICE = 2.5
+CAT_PACKET_PRICE = 4
 
 dog_packets_count = int(input())
 cat_packets_count = int(input())
-
-DOG_PACKET_PRICE = 2.5
-CAT_PACKET_PRICE = 4
 
 dog_food_price = dog_packets_count * DOG_PACKET_PRICE
 cat_food_price = cat_packets_count * CAT_PACKET_PRICE
