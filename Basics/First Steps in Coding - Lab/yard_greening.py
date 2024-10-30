089ddec3f2c9571dd7015519fa18ce1eff9fc570
mmilushev91 <milushev_1990@abv.bg>
2024-10-30 14:12:00 +0200

    Update yard_greening.py

--- Basics/First Steps in Coding - Lab/yard_greening.py
+++ Basics/First Steps in Coding - Lab/yard_greening.py
@@ -1,20 +1,12 @@
-# Pseudocode
+GREENING_SQ_M_PRICE = 7.61
+DISCOUNT = 0.18
 
-# prompt sq meters to be greened(float)
-# init const variable for price sq meter
-# calculate total price
-# calculate discount (18% from total price)
-# calculate final price
-# print first line: "The final price is: {крайна цена на услугата} lv."
-# print second line: "The discount is: {отстъпка} lv."
+sq_m = float(input())
 
-sq_meters = float(input())
+total_cost = sq_m * GREENING_SQ_M_PRICE
+discount =  total_cost * DISCOUNT
 
-PRICE_PER_SQ_METER = 7.61
-
-total_price = sq_meters * PRICE_PER_SQ_METER
-discount = total_price * 0.18
-final_price = total_price - discount
+final_price = total_cost - discount
 
 print(f"The final price is: {final_price} lv.")
 print(f"The discount is: {discount} lv.")
