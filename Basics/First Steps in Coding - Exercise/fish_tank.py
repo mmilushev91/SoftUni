d1e532d5754f486c9c07a443c4c018a83623bc29
mmilushev91 <milushev_1990@abv.bg>
2024-10-30 14:23:31 +0200

    Update fish_tank.py

--- Basics/First Steps in Coding - Exercise/fish_tank.py
+++ Basics/First Steps in Coding - Exercise/fish_tank.py
@@ -1,15 +1,11 @@
-SHOES_PERCENT = 0.4
-OUTFIT_PERCENT = 0.2
-BALL_PERCENT = 0.25
-ACCESSORIES = 0.2
+length = int(input())
+width = int(input())
+height = int(input())
+percent = float(input()) / 100
 
-yearly_tax = int(input())
+volume = (length * width * height) / 1000
+taken_space = volume * percent
 
-shoes = yearly_tax - (yearly_tax * SHOES_PERCENT)
-outfit = shoes - (shoes * OUTFIT_PERCENT)
-ball = outfit * BALL_PERCENT
-accessories = ball * ACCESSORIES
+liters = volume - taken_space
 
-total_cost = yearly_tax + shoes + outfit + ball + accessories
-
-print(total_cost)
+print(liters)
