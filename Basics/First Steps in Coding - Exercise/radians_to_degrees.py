f2904a7339fa899e657c99948cd8e9abd2a67fc6
mmilushev91 <milushev_1990@abv.bg>
2024-10-30 14:13:10 +0200

    Update radians_to_degrees.py

--- Basics/First Steps in Coding - Exercise/radians_to_degrees.py
+++ Basics/First Steps in Coding - Exercise/radians_to_degrees.py
@@ -1,10 +1,3 @@
-# Pseudocode
-
-# from math import pi
-# prompt for radians(float)
-# calculate degrees wtih градус = радиан * 180 / π
-# print result
-
 from math import pi
 
 radians = float(input())
