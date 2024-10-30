0f31aee26b09cd1e94e04213da6d806ccab36db6
mmilushev91 <milushev_1990@abv.bg>
2024-10-30 14:16:38 +0200

    Update deposit_calculator.py

--- Basics/First Steps in Coding - Exercise/deposit_calculator.py
+++ Basics/First Steps in Coding - Exercise/deposit_calculator.py
@@ -1,14 +1,9 @@
-# Pseudocode
+deposit = float(input())
+period = int(input())
+montly_interest_rate = (float(input()) / 100) / 12
 
-# prompt user for deposit amount(float), deposit interval(int), annual interest rate(float)
-# calculate final sum: сума = депозирана сума  + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
-# print result
+calculated_grow = (deposit * montly_interest_rate) * period
 
-deposit_amount = float(input())
-deposit_interval = int(input())
-annual_interest_rate = float(input()) / 100
+total_money = calculated_grow + deposit
 
-final_sum = deposit_amount + deposit_interval * \
-((deposit_amount * annual_interest_rate) / 12)
-
-print(final_sum)
+print(total_money)
