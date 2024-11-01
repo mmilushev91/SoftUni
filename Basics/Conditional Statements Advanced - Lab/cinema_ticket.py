c6b26e06f7dcf79008aced25f0296d5c9347bfda
mmilushev91 <milushev_1990@abv.bg>
2024-11-01 12:01:40 +0200

    Create cinema_ticket.py

---
+++ Basics/Conditional Statements Advanced - Lab/cinema_ticket.py
@@ -0,0 +1,13 @@
+DAYS = {
+    "Monday": 12,
+    "Tuesday": 12,
+    "Wednesday": 14,
+    "Thursday": 14,
+    "Friday": 12,
+    "Saturday": 16,
+    "Sunday": 16,
+}
+
+day = input()
+
+print(DAYS[day])
