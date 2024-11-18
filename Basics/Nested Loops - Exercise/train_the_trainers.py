30b168eee6ad6a949a50364f38aab5c7986ab431
mmilushev91 <milushev_1990@abv.bg>
2024-11-18 14:27:09 +0200

    Create train_the_trainers.py

---
+++ Basics/Nested Loops - Exercise/train_the_trainers.py
@@ -0,0 +1,21 @@
+START = 1111
+END = 9999
+
+n = int(input())
+
+for num in range(START, END + 1):
+  
+  num_string = str(num)
+  
+  is_special = True
+  
+  for digit in num_string:
+    int_digit = int(digit)
+    
+    if int_digit == 0 or n % int_digit != 0:
+      is_special = False
+      break
+    
+  if is_special:
+    print(num_string, end=" ")
+    
