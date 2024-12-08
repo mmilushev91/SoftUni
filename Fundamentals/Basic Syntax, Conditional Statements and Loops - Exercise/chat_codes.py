fa64f0d64a0a3798cf1b81dd2927ac25f6213049
mmilushev91 <milushev_1990@abv.bg>
2024-12-08 11:14:33 +0200

    Create chat_codes.py

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/chat_codes.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for _ in range(n):
+    num = int(input())
+
+    if num == 88:
+        print("Hello")
+    elif num == 86:
+        print("How are you?")
+    elif num < 88:
+        print("GREAT!")
+    else:
+        print("Bye.")
